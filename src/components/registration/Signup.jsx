8c7ac5b075aea07bcaf976af07dc53b3f5562a9c
mithleshkr <79351404+mithleshkr@users.noreply.github.com>
2022-08-12 15:22:41 +0530

    Update Signup.jsx

--- src/components/registration/Signup.jsx
+++ src/components/registration/Signup.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import React,{useState} from 'react'
+import "./Style.css";
+import { Button, TextField } from "@material-ui/core";
+import { auth,db } from '../../firebase';
+import { useNavigate } from 'react-router-dom';
+import swal from 'sweetalert';
 
 const Signup = () => {
+
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const handleSignup = () =>{
+    auth.createUserWithEmailAndPassword(email,password).then((result)=>{
+      setTimeout(()=>{
+        navigate('/')
+      },2000)
+      swal("GREAT!", "Successfully registered", "success")
+      
+    },(error)=>{
+      swal("Oops!", "Either email already registered or bad email", "error");
+      
+    })
+    db.collection("registration-details").add(
+      {
+        Name: name,
+        Email: email,
+        Password:password
+      }
+    )
+  }
+
   return (
-    <div>
-      <h1>This is Signup screen</h1>
+    <div className="main-div">
+      <div className="login-title">
+        <h2>Task Management</h2>
+      </div>
+      <div>
+        <card className="card">
+          <div className="card-title">
+            <h4>Sign up to your account</h4>
+            <br/>
+            <div className="input-div">
+            <TextField
+                type="text"
+                label="Name"
+                placeholder="Enter Your Name"
+                variant="outlined"
+                fullwidth
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <br />
+              <TextField
+                type="email"
+                label="Email"
+                placeholder="Enter Your Email"
+                variant="outlined"
+                fullwidth
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <br />
+              <TextField
+                type="password"
+                label="Password"
+                placeholder="Enter Your Password"
+                variant="outlined"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <br />
+              <div className="login-button">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="small"
+                  onClick={handleSignup}
+                  disabled={!name || !email || !password}
+                >
+                  Sign up
+                </Button>
+              </div>
+              
+              <br/>
+              
+            </div>
+          </div>
+        </card>
+      </div>
     </div>
   )
 }
