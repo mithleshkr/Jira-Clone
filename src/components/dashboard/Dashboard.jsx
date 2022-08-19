dbf0af50c1884bdbf871008dabb72d72927406e3
mithleshkr <79351404+mithleshkr@users.noreply.github.com>
2022-08-19 12:57:05 +0530

    Add files via upload

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,26 +1,24 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../header/Header";
 import Navbar from "../navbar/Navbar";
 import { Add } from "@material-ui/icons";
 import { Button } from "@material-ui/core";
 import "./Style.css";
 import "antd/dist/antd.css";
-import {  Form, Modal, Select,Input } from "antd";
+import { Form, Modal, Select, Input, Card, Col, Row } from "antd";
 import { db } from "../../firebase";
-import swal from 'sweetalert';
+import swal from "sweetalert";
 
 const CollectionCreateForm = ({ visible, onCreate, onCancel }) => {
-
-
-  const [taskTitle,setTaskTitle] = useState("");
+  const [taskTitle, setTaskTitle] = useState("");
   const [taskDesc, setTaskDesc] = useState("");
   const [endDate, setEndDate] = useState("");
   const [assignTo, setAssignTo] = useState("");
 
-  const title= <h4>Create New Task</h4>
-  
+  const title = <h4>Create New Task</h4>;
+
   const [form] = Form.useForm();
-  
+
   return (
     <Modal
       visible={visible}
@@ -29,25 +27,21 @@ const CollectionCreateForm = ({ visible, onCreate, onCancel }) => {
       cancelText="Cancel"
       onCancel={onCancel}
       onOk={() => {
-       
-        
         form
           .validateFields()
           .then((values) => {
-            db.collection("task-details").add(
-              {
-                TaskTitle: taskTitle,
-                TaskDescreption: taskDesc,
-                EndDate: endDate,
-                AssignTo: assignTo
-              }
-            )
+            db.collection("task-details").add({
+              TaskTitle: taskTitle,
+              TaskDescreption: taskDesc,
+              EndDate: endDate,
+              AssignTo: assignTo,
+            });
             swal({
               title: "Great",
               text: "Task Added Successfully",
               icon: "success",
               timer: "2000",
-              button: false    
+              button: false,
             });
             form.resetFields();
             onCreate(values);
@@ -55,27 +49,9 @@ const CollectionCreateForm = ({ visible, onCreate, onCancel }) => {
           .catch((info) => {
             console.log("Validate Failed:", info);
           });
-          
-        //   db.collection("task-details").add(
-        //     {
-        //       TaskTitle: taskTitle,
-        //       TaskDescreption: taskDesc,
-        //       EndDate: endDate,
-        //       AssignTo: assignTo
-        //     }
-        //   ).then((result)=>{
-        //     console.log(result)
-        //     swal("GREAT","Task added successfully", "success")
-        //   },(error)=>{
-        //     console.log(error)
-        //     swal("Oops!", error.message, "error");
-           
-           
-        //  })
       }}
     >
       <Form
-      
         form={form}
         layout="vertical"
         name="form_in_modal"
@@ -93,62 +69,67 @@ const CollectionCreateForm = ({ visible, onCreate, onCancel }) => {
             },
           ]}
         >
-          <Input placeholder="Enter Task Title"
-          value={taskTitle}
-          onChange={(e)=>setTaskTitle(e.target.value)}
-        
+          <Input
+            placeholder="Enter Task Title"
+            value={taskTitle}
+            onChange={(e) => setTaskTitle(e.target.value)}
           />
-          
+
           {/* <TextField variant="outlined" fullWidth size="small"  placeholder="Enter Task Title"/> */}
         </Form.Item>
         <Form.Item name="task description" label="Task Description">
-        <Input placeholder="Enter Task Description"
-        value={taskDesc}
-        onChange={(e)=>setTaskDesc(e.target.value)}
-        
-        />
-        {/* <TextField variant="outlined" fullWidth size="small"  placeholder="Enter Task Description" type="textarea"/> */}
-        </Form.Item>
-        <Form.Item name="end-date" label="End Date" 
-         rules={[
-          {
-            required: true,
-            message: "Please select end date",
-          },
-        ]}
-        >
-          <Input type="date"
-          value={endDate}
-          onChange={(e)=>setEndDate(e.target.value)}
+          <Input
+            placeholder="Enter Task Description"
+            value={taskDesc}
+            onChange={(e) => setTaskDesc(e.target.value)}
           />
-        {/* <TextField type="date" variant="outlined" size="small" fullWidth/> */}
-      </Form.Item>
-      <Form.Item
-            name="assign-to"
-            label="Assign to"
-            rules={[
-              {
-                required: true,
-                message: 'Select assignie',
-              },
-            ]}
-          > 
-          <Select defaultValue="select assign to" value={assignTo} onChange={(value)=>setAssignTo(value)}>
+          {/* <TextField variant="outlined" fullWidth size="small"  placeholder="Enter Task Description" type="textarea"/> */}
+        </Form.Item>
+        <Form.Item
+          name="end-date"
+          label="End Date"
+          rules={[
+            {
+              required: true,
+              message: "Please select end date",
+            },
+          ]}
+        >
+          <Input
+            type="date"
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+          />
+          {/* <TextField type="date" variant="outlined" size="small" fullWidth/> */}
+        </Form.Item>
+        <Form.Item
+          name="assign-to"
+          label="Assign to"
+          rules={[
+            {
+              required: true,
+              message: "Select assignie",
+            },
+          ]}
+        >
+          <Select
+            defaultValue="select assign to"
+            value={assignTo}
+            onChange={(value) => setAssignTo(value)}
+          >
             <Select.Option value="demo1">Demo1</Select.Option>
             <Select.Option value="demo2">Demo2</Select.Option>
             <Select.Option value="demo3">Demo3</Select.Option>
             <Select.Option value="demo4">Demo4</Select.Option>
           </Select>
-              
-              
-          </Form.Item>
-        
+        </Form.Item>
       </Form>
     </Modal>
   );
 };
 
 const Dashboard = () => {
+  const [displaydata, setDisplayData] = useState([]);
   const [visible, setVisible] = useState(false);
 
   const onCreate = (values) => {
@@ -156,25 +137,44 @@ const Dashboard = () => {
     setVisible(false);
   };
 
-  // const [open, setOpen] = React.useState(false);
-
-  // const handleClickOpen = () => {
-  //   setOpen(true);
-  // };
-
-  // const handleClose = () => {
-  //   setOpen(false);
-  // };
+  function Fetchdata() {
+    db.collection("task-details")
+      .get()
+      .then((snapshot) => {
+        if (snapshot.docs.length) {
+          snapshot.docs.forEach((doc) => {
+            setDisplayData((prev) => {
+              return [...prev, { data: doc.data(), id: doc.id }];
+            });
+          });
+        }
+        console.log(displaydata);
+      });
+  }
+  useEffect(() => {
+    Fetchdata();
+  }, []);
 
   return (
     <div>
       <Header />
-      <div style={{ display: "flex" }}>
-        <Navbar />
-        <div className="dashboard-card">
-          <card className="dashboard-card">
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div style={{ display: "flex" }}>
+          <Navbar />
+        </div>
+
+        <div style={{ width: "80%" }}>
+          {/* <card> */}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "flex-end",
+              justifyContent: "flex-end",
+              marginTop: "5px",
+            }}
+          >
             <Button
-            style={{backgroundColor:"#1890ff",color:"white"}}
+              style={{ backgroundColor: "#1890ff", color: "white" }}
               variant="contained"
               onClick={() => {
                 setVisible(true);
@@ -183,22 +183,47 @@ const Dashboard = () => {
               <Add />
               Add Task
             </Button>
-            <CollectionCreateForm
-        visible={visible}
-        onCreate={onCreate}
-        onCancel={() => {
-          setVisible(false);
-        }}
-      />
-          </card>
+          </div>
+          <CollectionCreateForm
+            visible={visible}
+            onCreate={onCreate}
+            onCancel={() => {
+              setVisible(false);
+            }}
+          />
+
+          <div>
+            <Row gutter={15}>
+              <Col span={8}>
+                <Card title="TO DO" bordered={false}>
+                  <div style={{ maxHeight: "67vh", overflow: "auto" }}>
+                    {displaydata.map((task) => (
+                      <div className="todo-div">
+                        <h3>{task.data.TaskTitle}</h3>
+                        <h5>{task.data.EndDate}</h5>
+                        <h5>{task.data.AssignTo}</h5>
+                        <p>{task.data.TaskDescreption}</p>
+                      </div>
+                    ))}
+                  </div>
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="IN PROGRESS" bordered={false}>
+                  going on task
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="DONE" bordered={false}>
+                  completed task
+                </Card>
+              </Col>
+            </Row>
+          </div>
+
+          {/* </card> */}
         </div>
       </div>
-      {/* <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Add Task Details</DialogTitle>
-        <DialogContent>
-          <form></form>
-        </DialogContent>
-      </Dialog> */}
     </div>
   );
 };
